1cb0bc099f7f274af95a8735cb307932e09d8ff4
varprabhlite <185780179+varprabhlite@users.noreply.github.com>
2025-07-15 13:22:58 +0530

    add preloader component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { useState } from 'react';
 import Landing from "./pages/landing/Landing";
+import Preloader from './pages/components/preloader/Preloader'; 
 
 export default function App() {
-  return <Landing />;
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handleEnter = () => {
+    setIsLoading(false);
+    // add music later
+  };
+
+  return isLoading ? (
+    <Preloader onEnter={handleEnter} />
+  ) : (
+    <Landing />
+  );
 }
