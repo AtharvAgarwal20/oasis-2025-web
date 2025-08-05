3b8a496b3787a4fa33e58f6a36209940cb3449eb
Ishaan02-Coder <ishaandaftari2@gmail.com>
2025-08-05 13:40:41 +0530

    Reg page

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,32 @@
-import { useState } from "react";
-import Landing from "./pages/landing/Landing";
+import { Routes, Route, useLocation } from "react-router-dom";
 import DrawingPreloader from "./pages/components/drawingPreloader/DrawingPreloader";
-import styles from "./App.module.scss";
+import Landing from "./pages/landing/Landing";
+import { useState } from "react";
+import Registration from "./pages/registration/Registration";
+import Instructions from "./pages/instructions/instructions";
 
 export default function App() {
   const [isLoading, setIsLoading] = useState(true);
+  const location = useLocation();
 
   const handleEnter = () => {
     setIsLoading(false);
-    // add music later
   };
 
   return (
-    <div className={styles.root}>
-      {isLoading ? <DrawingPreloader onEnter={handleEnter} /> : <Landing />}
-    </div>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          isLoading ? (
+            <DrawingPreloader onEnter={handleEnter} />
+          ) : (
+            <Landing />
+          )
+        }
+      />
+      <Route path="/register" element={<Registration />} />
+      <Route path="/instructions" element ={<Instructions/>}></Route>
+    </Routes>
   );
 }
---
+++ src/pages/registration/Registration.tsx
@@ -0,0 +1,13 @@
+import RegistrationForm from '../components/registration/registrationform/registrationform'
+
+import styles from './Registration.module.scss'
+import React from 'react'
+const Registration = () => {
+  return (
+    <div className='reg'>
+      <RegistrationForm></RegistrationForm>
+    </div>
+  )
+}
+
+export default Registration
