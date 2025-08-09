c64fe7127124f040ec826793ccdf352b8748ffb4
Pranjal Gupta <pranjalgupta58.97@gmail.com>
2025-08-09 19:56:48 +0530

    bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import DrawingPreloader from "./pages/components/drawingPreloader/DrawingPreloader";
 import Landing from "./pages/landing/Landing";
 import { useState } from "react";
 import Registration from "./pages/Registration/Registration";
 import Instructions from "./pages/Instructions/Instructions";
-import Events from "./pages/Events/events";
+import Events from "./pages/Events/Events";
 
 export default function App() {
   const [isLoading, setIsLoading] = useState(true);
-  const location = useLocation();
+  // const location = useLocation();
 
   const handleEnter = () => {
     setIsLoading(false);
