800d9eb6c7e940310d4813f6ae76e64c1a0d4a76
sumit9722 <f20240024@pilani.bits-pilani.ac.in>
2025-08-16 23:47:57 +0545

    main pull

--- src/pages/landing/Landing.tsx
+++ src/pages/landing/Landing.tsx
@@ -97,38 +97,38 @@ export default function Landing({ goToRegister }: LandingProps) {
       );
   }, []);
 
-  const [timeLeft, setTimeLeft] = useState({
-    days: 0,
-    hours: 0,
-    minutes: 0,
-    seconds: 0,
-  });
+  // const [timeLeft, setTimeLeft] = useState({
+  //   days: 0,
+  //   hours: 0,
+  //   minutes: 0,
+  //   seconds: 0,
+  // });
 
-  useEffect(() => {
-    const calculateTimeLeft = () => {
-      const now = new Date();
-      const difference = TARGET_DATE.getTime() - now.getTime();
+  // useEffect(() => {
+  //   const calculateTimeLeft = () => {
+  //     const now = new Date();
+  //     const difference = TARGET_DATE.getTime() - now.getTime();
 
-      if (difference <= 0) {
-        setTimeLeft({ days: 0, hours: 0, minutes: 0, seconds: 0 });
-        return;
-      }
+  //     if (difference <= 0) {
+  //       setTimeLeft({ days: 0, hours: 0, minutes: 0, seconds: 0 });
+  //       return;
+  //     }
 
-      const days = Math.floor(difference / (1000 * 60 * 60 * 24));
-      const hours = Math.floor(
-        (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-      );
-      const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-      const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+  //     const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+  //     const hours = Math.floor(
+  //       (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  //     );
+  //     const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+  //     const seconds = Math.floor((difference % (1000 * 60)) / 1000);
 
-      setTimeLeft({ days, hours, minutes, seconds });
-    };
+  //     setTimeLeft({ days, hours, minutes, seconds });
+  //   };
 
-    calculateTimeLeft();
-    const timerId = setInterval(calculateTimeLeft, 1000);
+  //   calculateTimeLeft();
+  //   const timerId = setInterval(calculateTimeLeft, 1000);
 
-    return () => clearInterval(timerId);
-  }, []);
+  //   return () => clearInterval(timerId);
+  // }, []);
 
   return (
     <>
@@ -207,11 +207,14 @@ export default function Landing({ goToRegister }: LandingProps) {
               ))}
             </div>
           </div>
-        <div className={styles.registerBtnContainer} onClick={goToRegister}>
-  <img src={registerBtn} className={styles.registerBtn} alt="Register" />
-  <div className={styles.registerBtnText}>Register</div>
-</div>
-
+          <div className={styles.registerBtnContainer} onClick={goToRegister}>
+            <img
+              src={registerBtn}
+              className={styles.registerBtn}
+              alt="Register"
+            />
+            <div className={styles.registerBtnText}>Register</div>
+          </div>
         </div>
       </div>
     </>
