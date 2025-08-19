2b87930f8dd9e999116ff9a2718d8ee82d9d64a4
sumit9722 <f20240024@pilani.bits-pilani.ac.in>
2025-08-20 04:02:21 +0545

    mobile view minor bug fix

--- src/Homepage.tsx
+++ src/Homepage.tsx
@@ -1,14 +1,20 @@
 import Landing from "./pages/landing/Landing";
 import DrawingPreloader from "./pages/components/drawingPreloader/DrawingPreloader";
+import useOverlayStore from "./utils/store";
 
 export default function Homepage({
   goToRegister,
 }: {
   goToRegister: () => void;
 }) {
+  const removeGif = useOverlayStore((state) => state.removeGif);
   return (
     <div>
-      <div style={{ zIndex: 50, position: "relative" }}>
+      <div
+        style={
+          removeGif ? { display: "none" } : { zIndex: 50, position: "relative" }
+        }
+      >
         <DrawingPreloader />
       </div>
       <div style={{ zIndex: 100, position: "relative" }}>
