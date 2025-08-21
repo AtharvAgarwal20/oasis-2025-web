236b886f16ddafdeb0efad24ca0ab77e80bc500d
sumit9722 <f20240024@pilani.bits-pilani.ac.in>
2025-08-22 04:18:50 +0545

    coming soon in home removed

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import DoorTransition from "./pages/components/page-transition/DoorTransition";
 import AboutUs from "./pages/aboutus/AboutUs";
 import Contact from "./pages/contact/ContactPage";
 import ComingSoon from "./pages/comingSoon/ComingSoon";
+import Navbar from "./pages/components/navbar/Navbar";
 
 export default function App() {
   const navigate = useNavigate();
@@ -19,7 +20,9 @@ export default function App() {
   const [currentPage, setCurrentPage] = useState<
     "home" | "register" | "events" | "aboutus" | "contact" | "comingSoon"
   >(
-    location.pathname === "/register"
+    location.pathname === "/"
+      ? "home"
+      : location.pathname === "/register"
       ? "register"
       : location.pathname === "/events"
       ? "events"
@@ -119,6 +122,8 @@ export default function App() {
           }
         />
       )}
+
+      <Navbar goToPage={goToPage} />
 
       {!isPreloading && currentPage === "events" && <ComingSoon />}
       {!isPreloading && currentPage === "aboutus" && <AboutUs />}
