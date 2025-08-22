279a74be922401a38464462ad4734fac695a6bf4
sumit9722 <f20240024@pilani.bits-pilani.ac.in>
2025-08-22 23:53:56 +0545

    temporary Ham added

---
+++ src/pages/components/ham/ham.tsx
@@ -0,0 +1,55 @@
+import styles from "./ham.module.scss";
+import hamX from "/svgs/landing/hamX.svg";
+import phone from "/svgs/landing/phone.svg";
+import homeIcon from "/svgs/landing/homeIcon.svg";
+import aboutusIcon from "/svgs/landing/aboutusIcon.svg";
+import eventIcon from "/svgs/landing/eventsIcon.svg";
+import oasisLogo from "/images/landing/oasisLogo.png";
+
+const navItems = [
+  { label: "Home", katakana: "ホーム", links: "/", icon: homeIcon },
+  {
+    label: "About Us",
+    katakana: "アバウト・アス",
+    links: "/aboutus",
+    icon: aboutusIcon,
+  },
+  { label: "Contact", katakana: "コンタクト", links: "/contact", icon: phone },
+  { label: "Events", katakana: "イベンツ", links: "/events", icon: eventIcon },
+];
+export default function Ham({
+  goToPage,
+}: {
+  goToPage: (path: string) => void;
+}) {
+  return (
+    <div className={styles.ham}>
+      <div className={styles.closeBtn}>
+        <img src={hamX} alt="Close" className={styles.closeIcon} />
+      </div>
+      <div className={styles.logoContainer}>
+        <img src={oasisLogo} alt="Oasis Logo" className={styles.logo} />
+      </div>
+      <div className={styles.linkContainer}>
+        {navItems.map((item, index) => (
+          <>
+            <div
+              key={item.label}
+              className={styles.navItem}
+              onClick={() => goToPage(item.links)}
+            >
+              <img src={item.icon} alt={item.label} className={styles.icon} />
+              <span className={styles.label}>{item.label}</span>
+            </div>
+            {index < navItems.length - 1 && <div className={styles.line}></div>}
+          </>
+        ))}
+      </div>
+      <div className={styles.mwd}>
+        {/* <img src={mwd} alt="Made with DVM" />
+         */}
+        Made with ❤️ by DVM
+      </div>
+    </div>
+  );
+}
