7bf6d98340b008732e9c38e62f840e060b9b86de
AnonymousTechDeveloper <anshmalviya54@gmail.com>
2025-08-24 18:45:55 +0530

    fixed triple digit number on time left

--- src/pages/landing/Landing.tsx
+++ src/pages/landing/Landing.tsx
@@ -408,7 +408,7 @@ export default function Landing({
             <div className={styles.dateCountdown} ref={dateCountdownRef}>
               <div className={`${styles.daysLeft} ${styles.timeLeft}`}>
                 <div className={styles.days}>
-                  {timeLeft.days > 10 ? (
+                  {timeLeft.days >= 10 ? (
                     <span>{timeLeft.days}</span>
                   ) : (
                     <span>0{timeLeft.days}</span>
@@ -419,7 +419,7 @@ export default function Landing({
               :
               <div className={`${styles.hoursLeft} ${styles.timeLeft}`}>
                 <div className={styles.hours}>
-                  {timeLeft.hours > 10 ? (
+                  {timeLeft.hours >= 10 ? (
                     <span>{timeLeft.hours}</span>
                   ) : (
                     <span>0{timeLeft.hours}</span>
@@ -430,7 +430,7 @@ export default function Landing({
               :
               <div className={`${styles.minutesLeft} ${styles.timeLeft}`}>
                 <div className={styles.minutes}>
-                  {timeLeft.minutes > 10 ? (
+                  {timeLeft.minutes >= 10 ? (
                     <span>{timeLeft.minutes}</span>
                   ) : (
                     <span>0{timeLeft.minutes}</span>
